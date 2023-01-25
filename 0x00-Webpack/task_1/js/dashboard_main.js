25d75efc56d8e92b8e2cce9f2f1384b01823fed8
robby10Z <85261447+robby10Z@users.noreply.github.com>
2023-01-25 23:46:08 +0000

    react intro
    react native

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,20 @@
+'use strict';
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+const updateCounter = () => {
+  let counts = $('#count').html() || 0;
+  $('button').on('click', () => {
+    counts++;
+    $('#count').html(`${counts} clicks on the button`);
+  });
+};
+
+_.debounce(updateCounter, 500);
+updateCounter();
